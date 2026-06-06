d3aa26f75a8fd12bf818c95dc9bdee5ccefb14b1
gvraghuveer <gvraghuveer07@gmail.com>
2026-06-06 21:31:54 +0530

    Add drawLine function to enable line drawing on canvas

--- main.c
+++ main.c
@@ -8,11 +8,13 @@ char canvas[ROWS][COLS];
 void initCanvas();
 void displayCanvas();
 void drawRectangle(int row, int col, int width, int height);
+void drawLine(int row1, int col1, int row2, int col2);
 
 int main()
 {
     initCanvas();
     drawRectangle(2, 5, 10, 4);
+    drawLine(8, 30, 15, 30);
     displayCanvas();
 
     return 0;
@@ -55,6 +57,37 @@ void drawRectangle(int row, int col, int width, int height)
     for (i = row; i < row + height; i++)
     {
         canvas[i][col + width - 1] = '*';
+    }
+}
+
+void drawLine(int row1, int col1, int row2, int col2){
+    int i;
+    if(row1 == row2){
+        if(col1 > col2){
+            int temp = col1;
+            col1 = col2;
+            col2 = temp;
+        }
+
+        for(i=col1; i<=col2; i++){
+            canvas[row1][i] = '*';
+        }
+    }
+
+    else if(col1 == col2){
+        if(row1 > row2){
+            int temp = row1;
+            row1 = row2;
+            row2 = temp;
+        }
+
+        for(i=row1; i<=row2; i++){
+            canvas[i][col1] = '*';
+        }
+    }
+
+    else{
+        printf("Horizontal and Vertical are accepted.");
     }
 }
 
